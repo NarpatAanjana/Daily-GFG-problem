7d0c5f671cee841a23afa98d38c3b4c070488a26
Narpat Aanjana <narpatanjana0@gmail.com>
2022-10-23 22:08:38 +0530

    23-10-2022

---
+++ Octomber/Stepping Numbers.cpp
@@ -0,0 +1,55 @@
+Class Solution{
+public:
+
+    int solve(int m)
+    {
+        if(m<0)
+        return 0;
+        
+        if(m<10)
+        return m+1;
+        
+        queue<int>q;
+        for(int i=1;i<=9;i++)
+            q.push(i);
+            
+        int ans=1;
+        while(q.size()>0)
+        {
+            int digit=q.front();
+            q.pop();
+            ans++;
+            
+            int rem=digit%10;
+            int num1=digit*10+rem-1;
+            int num2=digit*10+rem+1;
+            
+            if(rem==0)
+            {
+                if(num2<=m)
+                 q.push(num2);
+            }
+            else if (rem==9)
+            {
+                if(num1<=m)
+                    q.push(num1);
+            
+            }
+            else
+            {
+                 if(num1<=m)
+                    q.push(num1);
+                if(num2<=m)
+                    q.push(num2);
+            }
+        }
+        return ans;
+    }
+    int steppingNumbers(int n, int m)
+    {
+        // Code Here
+        return solve(m)-solve(n-1);
+    
+        
+    }
+};
