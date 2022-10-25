4614edebbb89515eb278ef3326d97836d883b8ae
Narpat Aanjana <narpatanjana0@gmail.com>
2022-10-25 22:03:14 +0530

    25-10-222

---
+++ Octomber/Maximum of all subarrays of size k.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        vector<int>ans;
+        int i=0,j=0;
+        deque<int>q;
+        
+        while(j<n)
+        {
+            
+            while(q.size()>0 && q.back()<arr[j])
+                q.pop_back();
+                
+            q.push_back(arr[j]);
+                
+                
+            if(j-i+1<k)
+               j++;
+             else if(j-i+1==k)
+            {
+                ans.push_back(q.front());
+                    
+                if(q.front()==arr[i])
+                 q.pop_front();
+                    
+                 i++;
+                 j++;
+            }
+        }
+        return ans;
+    }
+};
