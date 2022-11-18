daf89a6de72a66cf7b52499367a264876b297f4a
Narpat Aanjana <narpatanjana0@gmail.com>
2022-11-18 20:31:27 +0530

    18-11-2022

---
+++ November/Number Of Open Doors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    int noOfOpenDoors(long long n) {
+        
+        return floor(sqrt(n));
+        
+        int ans=0;
+        
+        vector<int> a(n+1,1);
+        
+        for(long long i=2;i<n+1;i++){
+            
+            long long s=1;
+            
+            while((i*s)<n+1){
+                if(a[i*s]){
+                    a[i*s]=0;
+                }
+                else{
+                    a[i*s]=1;
+                }
+                s++;
+            }
+        }
+        
+        for(long long i=1;i<n+1;i++){
+            if(a[i]){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
